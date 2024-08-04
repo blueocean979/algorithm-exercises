32d47ee0a6bc57c2f33f9b9cf172012465b914a6
ocean <sa.ocean97@gmail.com>
2024-08-04 12:06:51 +0330

    BST - find implemented

--- BST.js
+++ BST.js
@@ -2,6 +2,10 @@
 Please implement the find and contains to the following Binary Search Tree
 **/
 
+/**
+Please implement the find and contains to the following Binary Search Tree
+**/
+
 class Node {
     constructor(value){
         this.value = value;
@@ -41,8 +45,31 @@ class BinarySearchTree {
     find(value){
         // find the node based on the given value
         // return value should be the found node or undefined
+        if (this.root === null) return undefined
+        let current = this.root;
+        while(current !== null){
+            console.log(current, 'current')
+            if(value === current.value ) return current
+            else if (value > current.value) current = current.right
+            else current = current.left
+        }
+        return undefined
     }
     contains(value){
         // return true or false if the contains the given value or not
     }
 }
+
+const bst = new BinarySearchTree();
+bst.insert(10);
+bst.insert(5);
+bst.insert(15);
+bst.insert(3);
+bst.insert(7);
+bst.insert(12);
+bst.insert(18);
+bst.insert(32);
+console.log(bst)
+
+const findNodeRes = bst.find(32)
+console.log(findNodeRes, 'findNodeRes' )
