c11532720407631530c918ebfde0d29771fb9502
ocean <sa.ocean97@gmail.com>
2024-08-05 15:45:57 +0330

    queue: dequeue done

--- queue.js
+++ queue.js
@@ -30,6 +30,27 @@ class Queue {
     }
 
     dequeue(){
-        // return null or a value
+        if(this.size === 0) return null
+        let i = 1
+        const dequeued = this.last.value
+        let current = this.first; 
+        console.log(current)
+        while(i < this.size - 1){
+            current = current.next;
+            i++;
+        }
+        current.next = null
+        this.last = current;
+        this.size -=1;
+        return dequeued
     }
 }
+const queue = new Queue()
+queue.enqueue(1)
+queue.enqueue(2)
+queue.enqueue(3)
+queue.enqueue(4)
+console.log(queue)
+const dequeueRes = queue.dequeue()
+console.log(dequeueRes, 'dequeueRes')
+console.log(queue)
