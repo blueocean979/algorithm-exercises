cafb22da0928f9f17632ee795e6b0689b3a67bde
ocean <sa.ocean97@gmail.com>
2024-08-05 12:47:17 +0330

    DoublyLinkedList _ push_pop_shit_unshift

--- DoublyLinkedList.js
+++ DoublyLinkedList.js
@@ -3,8 +3,8 @@ Complete the following DoublyLinkedList
 **/
 
 
-class Node{
-    constructor(val){
+class Node {
+    constructor(val) {
         this.val = val;
         this.next = null;
         this.prev = null;
@@ -13,30 +13,71 @@ class Node{
 
 
 class DoublyLinkedList {
-    constructor(){
+    constructor() {
         this.head = null;
         this.tail = null;
         this.length = 0;
     }
-    push(val){
-        // code here
-    } 
-    pop(){
+    push(val) {
+        const newNode = new Node(val);
+        if (this.head === null) {
+            this.head = newNode;
+            this.tail = newNode
+            return this;
+        }
+        newNode.prev = this.tail;
+        this.tail.next = newNode;
+        this.tail = newNode
+    }
+    pop() {
+        if (this.head === null) return
+        const popped = this.tail;
+        if (popped.prev === null) {
+            this.head = null
+            this.tail = null
+        } else {
+            this.tail = popped.prev
+            this.tail.next = null
+        }
+        popped.next = null
+        popped.prev = null
+    }
+    shift() {
+        if (this.head === null) return;
+        if ( this.head === this.tail){
+            this.tail = null
+            this.head = null
+        } else if (this.head.next !== null){
+            const shifted = this.head;
+            this.head = this.head.next;
+            this.head.prev = null;
+            shifted.next = null
+        }
+
+    }
+    unshift(val) {
+        if (this.head === null) return
+        const newNode = new Node(val);
+        newNode.next = head;
+        this.head.prev = newNode;
+        this.head = newNode
+    }
+    get(index) {
+        
+    }
+    set(index, val) {
         // code here
     }
-    shift(){
+    insert(index, val) {
         // code here
-    }
-    unshift(val){
-        // code here
-    }
-    get(index){
-        // code here
-    }
-    set(index, val){
-      // code here
-    }
-    insert(index, val){
-      // code here
     }
 }
+
+const linkedList = new DoublyLinkedList()
+linkedList.push(3)
+linkedList.push(5)
+linkedList.push(8)
+linkedList.pop()
+linkedList.unshift(12)
+
+console.log(linkedList)
